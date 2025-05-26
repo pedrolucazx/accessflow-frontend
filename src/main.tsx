0a13164f067be390bb0ca08dda059d73b661be2a
pedrolucazx <pedrolucastrainee@gmail.com>
2025-05-25 22:03:39 -0300

    feat(apollo): wrap app with ApolloProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
 import App from './App.tsx';
+import './index.css';
+
+const client = new ApolloClient({
+  uri: import.meta.env.VITE_API_URL,
+  cache: new InMemoryCache(),
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </StrictMode>
 );
